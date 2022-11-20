5026162337f109b8501e14d6521e8eb29c5b8c35
Ivan Shvets <ivan.shvets@edisen.com>
2022-11-20 19:33:33 +0200

    Added Goerli deployment configuration

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const fs = require('fs');
+const provider = require('@truffle/hdwallet-provider');
+
+const secrets = JSON.parse(fs.readFileSync('.secrets.json').toString().trim());
 
 /**
  * Use this file to configure your truffle project. It's seeded with some
@@ -86,13 +90,16 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () =>
+        new provider(
+          secrets.privateKeys,
+          `https://goerli.infura.io/v3/${secrets.projectId}`,
+          0,
+          3
+        ),
+      network_id: 5, // Goerli's id
+    },
     //
     // Useful for private networks
     // private: {
